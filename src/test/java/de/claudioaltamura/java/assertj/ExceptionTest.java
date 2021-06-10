fdc5519fd058457cb8e8fd66460e74d12d3c8fbb
Claudio Altamura <claudio@claudioaltamura.de>
2021-06-10 07:14:55 +0200

    new: added more exception test methods

--- src/test/java/de/claudioaltamura/java/assertj/ExceptionTest.java
+++ src/test/java/de/claudioaltamura/java/assertj/ExceptionTest.java
@@ -9,17 +9,25 @@ class ExceptionTest {
   @Test
   void testException() {
     Car oldCar = new Car();
-    Throwable thrown = catchThrowable(() -> oldCar.explode());
-    assertThat(thrown).hasMessage("boom!");
+    Throwable thrown = catchThrowable(oldCar::explode);
+    assertThat(thrown).isInstanceOf(Exception.class).hasMessage("boom!");
   }
 
   @Test
-  public void testDoesNotThrowAnyException() {
+  void testDoesNotThrowAnyException() {
     Car car = new Car();
-    assertThatCode(
-            () -> {
-              car.hoot();
-            })
-        .doesNotThrowAnyException();
+    assertThatCode(car::hoot).doesNotThrowAnyException();
+  }
+
+  @Test
+  void testAssertThatThrownBy() {
+    Car oldCar = new Car();
+    assertThatThrownBy(oldCar::explode).isInstanceOf(Exception.class).hasMessageContaining("boom");
+  }
+
+  @Test
+  void testAssertThatExceptionOfType() {
+    Car oldCar = new Car();
+    assertThatExceptionOfType(Exception.class).isThrownBy(oldCar::explode);
   }
 }
